a5779ad85eb0b30acc112d47677b46ba6b526204
Andrew <browner12@gmail.com>
2016-03-04 21:57:45 -0600

    add scaffolding
    haven’t implemented these providers yet

--- src/AuthorizeBilling.php
+++ src/AuthorizeBilling.php
@@ -2,5 +2,17 @@
 
 class AuthorizeBilling implements BillingInterface
 {
-
+    /**
+     * charge
+     *
+     * @param int    $amount
+     * @param string $token
+     * @param string $description
+     * @return mixed
+     * @throws \browner12\billing\BillingException
+     */
+    public function charge($amount, $token, $description)
+    {
+        // TODO: Implement charge() method.
+    }
 }
